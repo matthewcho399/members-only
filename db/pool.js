36c63fd5f2700a60ca2de41b2fd919a595008a54
Matthew <matthewcho399@gmail.com>
2024-11-20 22:49:50 -0800

    db setup

---
+++ db/pool.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_LOCAL_URL,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to PostgreSQL:", err);
+  } else {
+    console.log("Successfully connected to PostgreSQL");
+  }
+});
+
+module.exports = pool;
---
+++ db/populatedb.js
@@ -0,0 +1,60 @@
+const { Client } = require("pg");
+require("dotenv").config();
+const { argv } = require("node:process");
+
+const usersTable = `
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    username VARCHAR (50) NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    membership_status BOOLEAN NOT NULL DEFAULT FALSE
+);`;
+
+const messagesTable = `
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR (100) NOT NULL,
+    text VARCHAR (500) NOT NULL,
+    added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`;
+
+const usersMessagesTable = `
+    user_id INTEGER,
+    message_id INTEGER
+`;
+
+async function main() {
+  try {
+    console.log("seeding...");
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_LOCAL_URL,
+    });
+
+    try {
+      await client.connect();
+      console.log("Connected to the database.");
+    } catch (err) {
+      console.error("Failed to connect:", err);
+      return;
+    }
+
+    try {
+      await client.query(usersTable);
+      await client.query(messagesTable);
+      await client.query(usersMessagesTable);
+      console.log("Database seeded successfully.");
+    } catch (err) {
+      console.error("Failed to execute query:", err);
+    } finally {
+      await client.end();
+      console.log("Connection closed.");
+    }
+  } catch (err) {
+    console.error("Unexpected error:", err);
+  }
+}
+
+main();
